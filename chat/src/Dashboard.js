033fd6a58ad7bb571e4edec41ac916c6c806abb1
dangolbeeker <scottb305@outlook.com>
2019-08-09 01:58:12 -0400

    Added Material UI List Components

--- chat/src/Dashboard.js
+++ chat/src/Dashboard.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -9,18 +13,27 @@ const useStyles = makeStyles(theme => ({
     padding: theme.spacing(3, 2),
   },
   flex: {
+    display: 'flex'
+  },
+
   topicsWindow: {
-width: '30%'
+    width: '30%',
+    height: '300px',
+    borderRight: '1px solid grey'
   },
+
   chatWindow: {
-width: '70%'
+    width: '70%'
+height: '300px',
   },
+
   chatBox: {
-width: '85%'
-height: '300px'
+    width: '85%'
+height: '300px',
+
   },
   button: {
-width: '15%'
+    width: '15%'
   }
 }));
 
@@ -32,15 +45,33 @@ export default function Dashboard() {
     <div>
       <Paper className={classes.root}>
         <Typography variant="h4" component="h4">
-         Chat app
+          Chat app
         </Typography>
         <Typography variant="h5" component="h5">
-         Topic plaeholder
+          Topic plaeholder
         </Typography>
         <div className={classes.flex}>
+          <div className={classes.topicsWindow}>
 
-        </div>
-      </Paper>
-    </div>
-  );
-}
+
+          </div>
+          <div className={classes.chatWindow}>
+
+          </div>
+
+          <div className={classes.flex}>
+            <div className={classes.chatBox}
+
+
+// </div>
+// <List>
+//               ['topic'].map(topic => (
+//     <ListItem key={topic}
+//   ))
+// </List>
+
+//           </div>
+//       </Paper>
+//     </div>
+//       );
+// }
